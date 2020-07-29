dad32550907f3926bd8004bdda0a3057a7a5a010
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-07-29 10:41:46 -0400

    done

--- main.js
+++ main.js
@@ -2,8 +2,50 @@
  * YOUR CODE HERE *
  ******************/
 
+const newPerson = (newFirstName = "Anonymous", newLastName = "Person", newAge, isMarried = false) => {
+  const person = {
+  firstName: newFirstName,
+  lastName: newLastName,
+  age: newAge,
+  married: isMarried,
+  
+  goingOn: function() {
+    return person.age + 1;
+  },
 
+  ageUp: function() {
+    return person.age += 1;
+  },
+  
+  getFullName: function() {
+    return `${this.firstName} ${this.lastName}`
+  },
 
+  marry: function(spouse) {
+    this.married = true;
+    spouse.married = true;
+    this.spouseName = spouse.getFullName();
+    spouse.spouseName = this.getFullName();
+  },
+
+  divorce: function(spouse) {
+    this.married = false;
+    spouse.married = false;
+      delete spouse.spouseName;
+      delete this.spouseName;
+  },
+
+}
+return person;
+};
+
+const person = newPerson('Bob', 'Smith', 14, false) //?
+
+person.goingOn() //?
+
+person.ageUp(); //?
+
+person.getFullName() //?
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
